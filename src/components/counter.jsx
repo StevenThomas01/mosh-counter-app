ee75bb0b0ce16945bafd9ac0d7e913d18e992ffa
Kiet <kietlo@gmail.com>
2019-08-17 21:59:33 +0100

    How pass an event argument by using arrow function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,7 +18,8 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -27,7 +28,7 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
