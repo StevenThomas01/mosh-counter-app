cf0fd88de9dad99f7a576b2048f5e865001e0fef
Kiet <kietlo@gmail.com>
2019-08-24 22:17:05 +0100

    Pass data back to counter via attribute name

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,13 +3,18 @@ import Counter from "./counter";
 
 class Counters extends Component {
   state = {
-    counters: [{ id: 1, value: 0 }, { id: 1, value: 0 }, { id: 1, value: 0 }]
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
   };
   render() {
     return (
       <div>
         {this.state.counters.map(c => (
-          <Counter key={c.id} />
+          <Counter key={c.id} value={c.value} />
         ))}
       </div>
     );
